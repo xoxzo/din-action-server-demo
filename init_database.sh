06fa16b555a92f00b7df3fd86fedb633969ebcb5
Akira Nonaka <nonaka@mac.com>
2017-05-17 16:31:32 +0900

    add init db script

---
+++ init_database.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sqlite3 db.sqlite3 <<EOF
+delete from din_action_dinaction;
+insert into din_action_dinaction (id, action_text) values (1,"playback http://anonaka.github.io/100baigaeshi.mp3");
+insert into din_action_dinaction (id, action_text) values (2,"playback http://anonaka.github.io/ppap-short.mp3");
+insert into din_action_dinaction (id, action_text) values (3,"playback http://anonaka.github.io/oshiokiyo.mp3");
+insert into din_action_dinaction (id, action_text) values (4,"transfer +81336042000");
+select * from din_action_dinaction;
+EOF
