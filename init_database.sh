eb6661d3320fc34e41c2ff31c180a2633273a636
野中 哲 <anonaka@nonakas-MacBook-Air.local>
2017-05-18 15:21:22 +0900

    fix style

--- init_database.sh
+++ init_database.sh
@@ -4,6 +4,7 @@ delete from din_action_dinaction;
 insert into din_action_dinaction (id, action_text) values (1,"playback http://anonaka.github.io/100baigaeshi.mp3");
 insert into din_action_dinaction (id, action_text) values (2,"playback http://anonaka.github.io/ppap-short.mp3");
 insert into din_action_dinaction (id, action_text) values (3,"playback http://anonaka.github.io/oshiokiyo.mp3");
-insert into din_action_dinaction (id, action_text) values (4,"transfer +81336042000");
+insert into din_action_dinaction (id, action_text) values (4,"playback http://anonaka.github.io/you-are-dead.mp3");
+insert into din_action_dinaction (id, action_text) values (5,"transfer +81336042000");
 select * from din_action_dinaction;
 EOF
